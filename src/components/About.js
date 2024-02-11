5d2cfaacea195103114a2acbe193a379ac6724e7
SaskiaVdZ <143086816+SaskiaVdZ@users.noreply.github.com>
2024-02-11 20:27:59 +0100

    add text remove margin banner

--- src/components/About.js
+++ src/components/About.js
@@ -8,10 +8,17 @@ function About() {
   return (
     <Container maxWidth="sm" style={{ marginTop: "0px" }}>
       <Grid container spacing={3} direction="column" justify="center" style={{ margin: "0px" }}>
-        <Grid item xs={12} style={{ margin: "0px" }}>
-
-          <Paper elevation={3} style={{ margin: "0px" }}>
+        <Grid item xs={12}>
+          <Paper elevation={3} xs={12} >
           <img src={Banner}/>
+          <Paper>
+            <Typography variant="h2" align="left" padding={5}>Hi! My name is Saskia.</Typography>
+            <Typography variant="h2" align="left" padding={5}>I am a curious junior front-end developer who just finished a full-time
+        accelerated vocational course at Sundsgården folkhögskola:  IT/Tech for women - Front end Development.
+        I&#39;m currently learning React and studying Flutter at The App brewery
+        and looking forward to dive into Data Visualisation D3.js at the New
+        York University, shortly.</Typography>
+          </Paper>
         
 
           </Paper>
