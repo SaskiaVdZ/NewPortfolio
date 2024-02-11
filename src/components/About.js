46b699cfcf7b29378c5a20b7f593fc974da46641
SaskiaVdZ <143086816+SaskiaVdZ@users.noreply.github.com>
2024-02-11 20:10:56 +0100

    add banner

--- src/components/About.js
+++ src/components/About.js
@@ -2,14 +2,16 @@ import Grid from "@mui/material/Grid";
 import Container from "@mui/material/Container";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
+import Banner from "./images/Banner.jpg";
 
 function About() {
   return (
     <Container maxWidth="sm" style={{ marginTop: "0px" }}>
-      <Grid container spacing={3} direction="column" justify="center">
-        <Grid item xs={12}>
-          <Paper elevation={3} style={{ backgroundColor: "pink" }}>
-            <img src="./images/banner.jpeg"/>
+      <Grid container spacing={3} direction="column" justify="center" style={{ margin: "0px" }}>
+        <Grid item xs={12} style={{ margin: "0px" }}>
+
+          <Paper elevation={3} style={{ margin: "0px" }}>
+          <img src={Banner}/>
         
 
           </Paper>
