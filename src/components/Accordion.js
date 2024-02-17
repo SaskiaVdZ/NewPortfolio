953fb56f3da8029962d48ef067b99cefe5cd5efa
SaskiaVdZ <143086816+SaskiaVdZ@users.noreply.github.com>
2024-02-17 18:14:03 +0100

    three cards with accordion

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -9,19 +9,27 @@ import Banner from "./images/small.png";
 import Stack from "@mui/material/Stack";
 import Card from "@mui/material/Card";
 import Typography from "@mui/material/Typography";
+import Data from "../Data.json";
 
 export default function AccordionProjects() {
   return (
+
     <div>
+        
       <Card variant="elevation" elevation={3} style={{ margin: "20px" }}><img src={Banner} alt="Floating components"/>
-      <Accordion defaultExpanded>
+      <Accordion>
         <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
           aria-controls="panel1-content"
           id="panel1-header"
         >
             <Stack spacing={2}>
-          <Typography variant="h5">Drag and Drop Game - React</Typography>
+          <Typography variant="h5">Drag and Drop Game | React</Typography>
+          <Typography>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+          <ExpandMoreIcon />
           </Stack>
         </AccordionSummary>
         <AccordionDetails>
@@ -46,48 +54,96 @@ export default function AccordionProjects() {
           </Stack>
         </AccordionDetails>
         <AccordionActions>
-          <Button>Code</Button>
-          <Button>Live</Button>
+          <Button variant="outlined" size="small">Code</Button>
+          <Button variant="outlined" size="small">Live</Button>
         </AccordionActions>
       </Accordion>
       </Card>
 
       <Card variant="elevation" elevation={3} style={{ margin: "20px" }}><img src={Banner} alt="Floating components"/>
-      <Accordion defaultExpanded>
+      <Accordion>
         <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
           aria-controls="panel2-content"
           id="panel2-header"
         >
-          Income & Expense tracker - Javascript
+            <Stack spacing={2}>
+          <Typography variant="h5">Income & expense tracker | Javascript</Typography>
+          <Typography>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+          <ExpandMoreIcon />
+          </Stack>
         </AccordionSummary>
         <AccordionDetails>
+        <Stack spacing={2}>
+        <Typography>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
           malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+          <Typography>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+          <Typography>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+          <Typography>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+          </Stack>
         </AccordionDetails>
         <AccordionActions>
-          <Button>Code</Button>
-          <Button>Live</Button>
+          <Button variant="outlined" size="small">Code</Button>
+          <Button variant="outlined" size="small">Live</Button>
         </AccordionActions>
       </Accordion>
       </Card>
 
       <Card variant="elevation" elevation={3} style={{ margin: "20px" }}><img src={Banner} alt="Floating components"/>
-      <Accordion defaultExpanded>
+      <Accordion>
         <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
           aria-controls="panel3-content"
           id="panel3-header"
         >
-          Genesis - HTML, CSS, Sass
+            <Stack spacing={2}>
+          <Typography variant="h5">Genesis | HTML, CSS, Sass</Typography>
+          <Typography>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+          <ExpandMoreIcon />
+          </Stack>
         </AccordionSummary>
         <AccordionDetails>
+        <Stack spacing={2}>
+        <Typography>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
           malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+          <Typography>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+          <Typography>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+          <Typography>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+          </Stack>
         </AccordionDetails>
         <AccordionActions>
-          <Button>Code</Button>
-          <Button>Live</Button>
+          <Button variant="outlined" size="small">Code</Button>
+          <Button variant="outlined" size="small">Live</Button>
         </AccordionActions>
       </Accordion>
       </Card>
