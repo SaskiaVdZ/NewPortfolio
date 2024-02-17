4d78167c4effe454b6fb90b67b60c4ecab49ba2a
SaskiaVdZ <143086816+SaskiaVdZ@users.noreply.github.com>
2024-02-17 19:24:08 +0100

    link cards to database

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -6,28 +6,36 @@ import AccordionDetails from '@mui/material/AccordionDetails';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Button from '@mui/material/Button';
 import Banner from "./images/small.png";
+import Grid from "@mui/material/Grid";
 import Stack from "@mui/material/Stack";
 import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import Data from "../Data.json";
 
 export default function AccordionProjects() {
   return (
 
-    <div>
-        
-      <Card variant="elevation" elevation={3} style={{ margin: "20px" }}><img src={Banner} alt="Floating components"/>
+    
+    <Grid container>
+        {Data.map((result,index)=>(
+            <Grid item key={index}>
+      <Card variant="elevation" elevation={3} style={{ margin: "20px" }}>
+      <CardMedia
+                component="img"
+                height="140"
+                image={result.img}
+                alt="project image"
+              />
       <Accordion>
         <AccordionSummary
           aria-controls="panel1-content"
           id="panel1-header"
         >
             <Stack spacing={2}>
-          <Typography variant="h5">Drag and Drop Game | React</Typography>
+          <Typography variant="h5">{result.title}</Typography>
           <Typography>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          malesuada lacus ex, sit amet blandit leo lobortis eget.
+          {result.intro}
           </Typography>
           <ExpandMoreIcon />
           </Stack>
@@ -35,21 +43,16 @@ export default function AccordionProjects() {
         <AccordionDetails>
         <Stack spacing={2}>
         <Typography>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          malesuada lacus ex, sit amet blandit leo lobortis eget.
+        {result.par1}
           </Typography>
           <Typography>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          malesuada lacus ex, sit amet blandit leo lobortis eget.
+          {result.par2}
           </Typography>
           <Typography>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          malesuada lacus ex, sit amet blandit leo lobortis eget.
+          {result.par3}
           </Typography>
           <Typography>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          malesuada lacus ex, sit amet blandit leo lobortis eget.
+          {result.par4}
           </Typography>
           </Stack>
         </AccordionDetails>
@@ -59,95 +62,9 @@ export default function AccordionProjects() {
         </AccordionActions>
       </Accordion>
       </Card>
-
-      <Card variant="elevation" elevation={3} style={{ margin: "20px" }}><img src={Banner} alt="Floating components"/>
-      <Accordion>
-        <AccordionSummary
-          aria-controls="panel2-content"
-          id="panel2-header"
-        >
-            <Stack spacing={2}>
-          <Typography variant="h5">Income & expense tracker | Javascript</Typography>
-          <Typography>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-          <ExpandMoreIcon />
-          </Stack>
-        </AccordionSummary>
-        <AccordionDetails>
-        <Stack spacing={2}>
-        <Typography>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-          <Typography>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-          <Typography>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-          <Typography>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-          </Stack>
-        </AccordionDetails>
-        <AccordionActions>
-          <Button variant="outlined" size="small">Code</Button>
-          <Button variant="outlined" size="small">Live</Button>
-        </AccordionActions>
-      </Accordion>
-      </Card>
-
-      <Card variant="elevation" elevation={3} style={{ margin: "20px" }}><img src={Banner} alt="Floating components"/>
-      <Accordion>
-        <AccordionSummary
-          aria-controls="panel3-content"
-          id="panel3-header"
-        >
-            <Stack spacing={2}>
-          <Typography variant="h5">Genesis | HTML, CSS, Sass</Typography>
-          <Typography>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-          <ExpandMoreIcon />
-          </Stack>
-        </AccordionSummary>
-        <AccordionDetails>
-        <Stack spacing={2}>
-        <Typography>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-          <Typography>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-          <Typography>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-          <Typography>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-          </Stack>
-        </AccordionDetails>
-        <AccordionActions>
-          <Button variant="outlined" size="small">Code</Button>
-          <Button variant="outlined" size="small">Live</Button>
-        </AccordionActions>
-      </Accordion>
-      </Card>
-
-    </div>
+      </Grid>
+      ))}
+      </Grid>
+    
   );
 }
