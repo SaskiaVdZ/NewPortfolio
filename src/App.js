7d620271bc5c7cb74e494fd1695f68b047ed2cbc
SaskiaVdZ <143086816+SaskiaVdZ@users.noreply.github.com>
2024-02-27 12:18:07 +0100

    add borderRadius to theme

--- src/App.js
+++ src/App.js
@@ -3,21 +3,16 @@ import "@fontsource/inter/400.css";
 import "@fontsource/merriweather/300.css";
 import "./App.css";
 import ResponsiveAppBar from "./components/NavBar";
-import About from './components/About';
+import About from "./components/About";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
-import Experience from './components/Experience';
+import Experience from "./components/Experience";
 import Container from "@mui/material/Container";
 
-import {
-  createTheme,
-  ThemeProvider,
-} from "@mui/material/styles";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import ContactFooter from "./components/Contact";
 
-
 const theme = createTheme({
-  
   palette: {
     primary: {
       main: "#FFFFF", //White (background navbar)
@@ -25,44 +20,40 @@ const theme = createTheme({
       accent2: "#E8FDF5", //Washed-green
       accent3: "#FFDFDF", //Washed-Red
       accent4: "#E0DAFC", //Light purple
-      accent5:"#9CE5CA", //Mint-green
+      accent5: "#9CE5CA", //Mint-green
       accent6: "#FFD700", // Dark-yellow
     },
-  
+
     text: {
-      primary:"#E7040F",
-    }
+      primary: "#E7040F",
+    },
   },
- 
+
   typography: {
     allVariants: {
-      color: "#E7040F"
+      color: "#E7040F",
     },
     fontFamily: ["Inter"],
   },
-  
-  paper: { 
-    elevation: 0, borderRadius: 20, borderColor: "#000", padding: 50 },
 
   shape: {
-    borderadius: 16,
-  }
+    borderRadius: 16,
+  },
 });
-
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Container className="App"> 
-          <ResponsiveAppBar />
-          <Routes>
-            <Route path="/" element={<About />} />
-            <Route path="About" element={<About />} />
-            <Route path="Projects" element={<Projects />} />
-            <Route path="Experience" element={<Experience />} />
-            <Route path="Skills" element={<Skills />} />
-          </Routes>
-          <ContactFooter />
+      <Container className="App">
+        <ResponsiveAppBar />
+        <Routes>
+          <Route path="/" element={<About />} />
+          <Route path="About" element={<About />} />
+          <Route path="Projects" element={<Projects />} />
+          <Route path="Experience" element={<Experience />} />
+          <Route path="Skills" element={<Skills />} />
+        </Routes>
+        <ContactFooter />
       </Container>
     </ThemeProvider>
   );
