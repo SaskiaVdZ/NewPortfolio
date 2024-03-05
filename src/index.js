baf514398fe2605003d444068983fb6daa98f7fa
SaskiaVdZ <143086816+SaskiaVdZ@users.noreply.github.com>
2024-03-05 19:53:32 +0100

    change back to original homepage name, replace Browserrouter to HashRouter in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -8,11 +8,11 @@ import 'normalize.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <HashRouter basename={process.env.PUBLIC_URL}>
   <React.StrictMode>
-    <BrowserRouter>
     <App />
-    </BrowserRouter>
   </React.StrictMode>
+  </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
